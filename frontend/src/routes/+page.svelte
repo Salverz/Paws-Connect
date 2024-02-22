664b97b60a0be1803228b3fc8f7af0d429a20a8d
David <davidorona2@gmail.com>
2024-02-22 15:34:53 -0800

    Initialized svelte and express

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+import { onMount } from "svelte";
+import { apiData} from './store.js';
+
+onMount(async () => {
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Bourbon")
+  .then(response => response.json())
+  .then(data => {
+		console.log(data);
+    apiData.set(data);
+  }).catch(error => {
+    console.log(error);
+    return [];
+  });
+});
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
