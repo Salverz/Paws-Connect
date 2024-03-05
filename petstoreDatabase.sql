897844fdffe2016e85fe5eb3579d0c67fcf4cf07
RhysCrowell <157551589+RhysCrowell@users.noreply.github.com>
2024-03-04 22:34:29 -0800

    Update petstoreDatabase.sql

--- petstoreDatabase.sql
+++ petstoreDatabase.sql
@@ -6,19 +6,24 @@ DROP TABLE IF EXISTS breed;
 DROP TABLE IF EXISTS user;
 SET REFERENTIAL_INTEGRITY TRUE;
 
-CREATE TABLE user (
-    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE userProfile (
+    ID INT NOT NULL PRIMARY KEY,
     username VARCHAR(250) NOT NULL,
-    passcode VARCHAR(250) NOT NULL,
     email VARCHAR(250) NOT NULL,
     birthDate DATE,
     displayname VARCHAR(250),
     isAdmin BOOLEAN,
     chosenLocation VARCHAR(250),
-    selectedLanguage VARCHAR(100)
+    selectedLanguage VARCHAR(100),
+    FOREIGN KEY (ID) REFERENCES account(ID)
 );
-
-CREATE TABLE pets (
+CREATE TABLE account (
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(250) NOT NULL,
+    passcode VARCHAR(250) NOT NULL, 
+    email VARCHAR(250) NOT NULL,  
+);
+CREATE TABLE petsProfile (
     petsID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     userID INT, 
     petname VARCHAR(250) NOT NULL,
