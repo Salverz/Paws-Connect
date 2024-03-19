dc16ade21f2f2520d194cd56da444ff3e201b07c
David <davidorona2@gmail.com>
2024-03-19 14:51:54 -0700

    changed routing names

--- petstoreDatabase.sql
+++ petstoreDatabase.sql
@@ -15,41 +15,41 @@ DROP TABLE IF EXISTS user_account;
 DROP TABLE IF EXISTS language;
 SET REFERENTIAL_INTEGRITY TRUE;
 
-
 CREATE TABLE user_profile (
     user_id INT,
     birth_date DATE,
     display_name VARCHAR(32),
     location VARCHAR(32),
-    preferred_language CHAR(2),
+    preferred_language_code CHAR(4),
     FOREIGN KEY (user_id) REFERENCES account(user_id),
-	FOREIGN KEY (preferred_language) REFERENCES language(language_code)
+	FOREIGN KEY (preferred_language_code) REFERENCES language(language_code)
 );
+
 CREATE TABLE user_account (
     user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(32) NOT NULL,
-    password VARCHAR(256) NOT NULL, 
-    email VARCHAR(128) NOT NULL,  
+    password VARCHAR(256) NOT NULL,
+    email VARCHAR(128) NOT NULL,
 	is_admin BOOLEAN NOT NULL,
 );
+
 CREATE TABLE pet_profile (
     pet_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id INT, 
+    user_id INT,
     name VARCHAR(250) NOT NULL,
     profile_picture_link VARCHAR(256),
-    breed VARCHAR(64), 
+    breed VARCHAR(64),
     color VARCHAR(16),
     age INT,
     FOREIGN KEY (user_id) REFERENCES user_account(user_id),
-    FOREIGN KEY (preferred_language) REFERENCES language(language_code)
 );
 
 CREATE TABLE connection (
-    user_id_1 INT NOT NULL,
-    user_id_2 INT NOT NULL,
-    PRIMARY KEY (user_id_1, user_id_2),
-    FOREIGN KEY (user_id_1) REFERENCES user_account(user_id),
-    FOREIGN KEY (user_id_2) REFERENCES user_account(user_id)
+    user_1_id INT NOT NULL,
+    user_2_id INT NOT NULL,
+    PRIMARY KEY (user_1_id, user_2_id),
+    FOREIGN KEY (user_1_id) REFERENCES user_account(user_id),
+    FOREIGN KEY (user_2) REFERENCES user_account(user_id)
 );
 
 CREATE TABLE language (
@@ -65,7 +65,7 @@ CREATE TABLE post (
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     post_language_code CHAR(4),
     FOREIGN KEY (user_id) REFERENCES user_account(user_id),
-    FOREIGN KEY (post_language) REFERENCES language(language_code)
+    FOREIGN KEY (post_language_code) REFERENCES language(language_code)
 );
 
 CREATE TABLE post_photo (
@@ -100,7 +100,6 @@ CREATE TABLE activity_log (
     FOREIGN KEY (user_id) REFERENCES user_profile(user_id)
 );
 
-
 CREATE TABLE notification (
     notification_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
@@ -111,7 +110,6 @@ CREATE TABLE notification (
     FOREIGN KEY (user_id) REFERENCES user_profile(user_id)
 );
 
-
 CREATE TABLE search_history (
     search_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
@@ -119,7 +117,6 @@ CREATE TABLE search_history (
     searchDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES user_profile(user_id)
 );
-
 
 CREATE TABLE post_like (
     like_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -129,7 +126,6 @@ CREATE TABLE post_like (
     FOREIGN KEY (post_id) REFERENCES posts(post_id),
     FOREIGN KEY (user_id) REFERENCES user_profile(user_id)
 );
-
 
 CREATE TABLE comment (
     comment_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
