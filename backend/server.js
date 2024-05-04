5e163adf79deffb9905f2998f407a274531dfa69
Salverz <davidorona2@gmail.com>
2024-05-03 20:51:34 -0700

    Add dotenv

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
+require('dotenv').config();
 
 // Session stuff
 const session = require('express-session');
